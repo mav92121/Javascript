6314373c5dcf9c686a20b41de9fd0f296e1dc615
MAV <mohsinali530121@gmail.com>
2022-08-15 11:38:47 +0530

    light on off using js

---
+++ Light_On_Off/HTML CSS JS/index.js
@@ -0,0 +1,19 @@
+var On=document.getElementById('on');
+var Off=document.getElementById('off');
+var img=document.getElementById('main');
+var e=document.getElementById('exp');
+var li=document.getElementById('light');
+var da=document.getElementById('dark');
+On.addEventListener('click',()=>
+{
+   img.src='../Images/light_on.jpg';
+})
+Off.addEventListener('click',()=>
+{
+   img.src='../Images/light_off.jpg';
+})
+li.addEventListener('click',()=>
+{
+    document.getElementsByTagName('div')
+    // document.getElementsByTagName('h1').in
+})
